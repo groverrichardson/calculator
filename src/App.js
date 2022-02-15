8a48653019b45da1ac8cec631e97593a1b87a6e3
Grover Richardson <groverrichardson1987@gmail.com>
2022-02-15 15:04:27 -0800

    Moved calculator component out of App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import React from 'react';
-import CalcButton from './components/calcButton';
-import Display from './components/display';
+import Calculator from './components/calculator';
 
 function App() {
     return (
         <main className="App grid justify-center">
-            <Display />
-            <div className="calc-button-group grid grid-cols-4">
-                <CalcButton classes="clear bg-gray-500" display="AC" />
-                <CalcButton classes="pos-neg bg-gray-500" display="+/-" />
-                <CalcButton classes="percentage bg-gray-500" display="%" />
-                <CalcButton classes="division bg-orange-400" display="÷" />
-            </div>
-            <div className="calc-button-group grid grid-cols-4">
-                <CalcButton classes="division bg-gray-400" display="7" />
-                <CalcButton classes="division bg-gray-400" display="8" />
-                <CalcButton classes="division bg-gray-400" display="9" />
-                <CalcButton classes="division bg-orange-400" display="×" />
-            </div>
-            <div className="calc-button-group grid grid-cols-4">
-                <CalcButton classes="division bg-gray-400" display="4" />
-                <CalcButton classes="division bg-gray-400" display="5" />
-                <CalcButton classes="division bg-gray-400" display="6" />
-                <CalcButton classes="division bg-orange-400" display="-" />
-            </div>
-            <div className="calc-button-group grid grid-cols-4">
-                <CalcButton classes="division bg-gray-400" display="1" />
-                <CalcButton classes="division bg-gray-400" display="2" />
-                <CalcButton classes="division bg-gray-400" display="3" />
-                <CalcButton classes="division bg-orange-400" display="+" />
-            </div>
-            <div className="calc-button-group grid grid-cols-4">
-                <CalcButton classes="col-span-2 bg-gray-400" display="0" />
-                <CalcButton classes="division bg-gray-400" display="." />
-                <CalcButton classes="division bg-orange-400" display="=" />
-            </div>
+            <Calculator />
         </main>
     );
 }
---
+++ src/components/calculator.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import CalcButton from './calcButton';
+import Display from './display';
+
+function Calculator() {
+    return (
+        <div className="Calculator">
+            <Display />
+            <div className="calc-button-group grid grid-cols-4">
+                <CalcButton classes="clear bg-gray-500" display="AC" />
+                <CalcButton classes="pos-neg bg-gray-500" display="+/-" />
+                <CalcButton classes="percentage bg-gray-500" display="%" />
+                <CalcButton classes="division bg-orange-400" display="÷" />
+            </div>
+            <div className="calc-button-group grid grid-cols-4">
+                <CalcButton classes="division bg-gray-400" display="7" />
+                <CalcButton classes="division bg-gray-400" display="8" />
+                <CalcButton classes="division bg-gray-400" display="9" />
+                <CalcButton classes="division bg-orange-400" display="×" />
+            </div>
+            <div className="calc-button-group grid grid-cols-4">
+                <CalcButton classes="division bg-gray-400" display="4" />
+                <CalcButton classes="division bg-gray-400" display="5" />
+                <CalcButton classes="division bg-gray-400" display="6" />
+                <CalcButton classes="division bg-orange-400" display="-" />
+            </div>
+            <div className="calc-button-group grid grid-cols-4">
+                <CalcButton classes="division bg-gray-400" display="1" />
+                <CalcButton classes="division bg-gray-400" display="2" />
+                <CalcButton classes="division bg-gray-400" display="3" />
+                <CalcButton classes="division bg-orange-400" display="+" />
+            </div>
+            <div className="calc-button-group grid grid-cols-4">
+                <CalcButton classes="col-span-2 bg-gray-400" display="0" />
+                <CalcButton classes="division bg-gray-400" display="." />
+                <CalcButton classes="division bg-orange-400" display="=" />
+            </div>
+        </div>
+    );
+}
+
+export default Calculator;
