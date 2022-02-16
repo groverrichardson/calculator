f330d0b1c0123c00ca88b66c4c7538b3f8a9f1da
Grover Richardson <groverrichardson1987@gmail.com>
2022-02-15 16:23:06 -0800

    Removed console.log

--- src/components/display.js
+++ src/components/display.js
@@ -4,8 +4,6 @@ import { useApp } from '../AppContext';
 function Display() {
     const currentCalc = useApp();
 
-    console.log(currentCalc);
-
     return (
         <div className="Display text-right border-2 border-black py-5 px-5 bg-gray-700 text-white text-bold text-2xl">
             {currentCalc}
