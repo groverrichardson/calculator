13d01c1d99196de77fe9a21545abc44c68c9599a
Grover Richardson <groverrichardson1987@gmail.com>
2022-02-18 14:56:51 -0800

    Reset all function.

--- src/components/calcButton.js
+++ src/components/calcButton.js
@@ -1,13 +1,4 @@
 import React from 'react';
-import {
-    useCurrentVal,
-    useInstructions,
-    useReset,
-    useUpdateCurrentCalc,
-    useUpdateCurrentVal,
-    useUpdateInstructions,
-    useUpdateReset,
-} from '../AppContext';
 
 function CalcButton(props) {
     const numbers = new Set([
@@ -23,32 +14,8 @@ function CalcButton(props) {
         '0',
         '.',
     ]);
-    const updateCurrentVal = useUpdateCurrentVal();
-    const currentVal = useCurrentVal();
-    const updateCurrentCalc = useUpdateCurrentCalc();
-    const updateInstructions = useUpdateInstructions();
-    const reset = useReset();
-    const updateReset = useUpdateReset();
 
-    function handleClick(e) {
-        const input = e.target.value;
-        const newValue =
-            currentVal === '0' || reset === true ? input : currentVal + input;
-
-        updateReset(false);
-
-        if (numbers.has(input)) {
-            updateCurrentVal(newValue);
-            updateInstructions('inputting');
-        }
-
-        if (input === '+') updateInstructions('add');
-        if (input === '-') updateInstructions('subtract');
-        if (input === '×') updateInstructions('multiply');
-        if (input === '÷') updateInstructions('divide');
-        if (input === '=') updateInstructions('process');
-        if (input === 'AC') updateInstructions('clear');
-    }
+    function handleClick(e) {}
 
     return (
         <button
--- src/components/display.js
+++ src/components/display.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { useCurrentCalc, useCurrentVal, useStagedVal } from '../AppContext';
 
-function Display() {
-    const currentVal = useCurrentVal();
-    const stagedVal = useStagedVal();
-    const currentCalc = useCurrentCalc();
-
+function Display(props) {
     return (
         <div className="Display text-right border-2 border-black py-5 px-5 bg-gray-700 text-white text-bold text-2xl">
             {currentVal === '0' ? currentCalc : currentVal}
