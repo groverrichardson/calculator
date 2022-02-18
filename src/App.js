bc4f106cd45f466f721c834a43d11f65f71b3f6e
Grover Richardson <groverrichardson1987@gmail.com>
2022-02-18 15:14:58 -0800

    Updated UI for styling for prev and current values.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Calculator from './components/calculator';
-import { AppProvider } from './AppContext';
 
 function App() {
     return (
-        <AppProvider>
-            <main className="App grid justify-center">
-                <Calculator />
-            </main>
-        </AppProvider>
+        <main className="App grid justify-center">
+            <Calculator />
+        </main>
     );
 }
 
--- src/components/display.js
+++ src/components/display.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Display(props) {
     return (
-        <div className="Display text-right border-2 border-black py-5 px-5 bg-gray-700 text-white text-bold text-2xl">
-            {currentVal === '0' ? currentCalc : currentVal}
+        <div className="display-container text-right border-2 border-black py-5 px-5 bg-gray-700 text-white text-bold text-2xl">
+            <p className="prev-value text-sm">Prev Value</p>
+            <p className="current-value text-4xl ">test</p>
         </div>
     );
 }
